4cbc36637fa84817e02d5969178ddab868d3fc26
Pravesh Kunwar <praveshk04@outlook.com>
2021-05-03 22:21:30 -0400

    feat: learning functions.

--- learning/src/main.rs
+++ learning/src/main.rs
@@ -1,4 +1,4 @@
-
+#![allow(unused_variables)]
 fn main() {
 
     let mut x  = 5;
@@ -8,4 +8,20 @@ fn main() {
     const MAX: i32 = 10000;
     println!("The max points you can get is {}", MAX);
     let y = x * 2;
+    
+    //data types
+    let a:  [i32; 5] = [1, 2, 3, 4, 5];
+    println!("Values of a are {}", a[0]);
+    let z: f32 = 1.0;
+    let t = true;
+
+    let f: bool = false;
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+    let tuple: (i32, f32, i32) = (1, 2.5, 3);
+    println!("Adding the numbers, we got: {}", add(5, 3))
+}
+
+fn add(x: i32, y: i32) -> i32 {
+    let result = x + y;
+    return result;
 }
